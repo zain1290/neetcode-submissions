d4effe8c0708445be8c9cb3a763b2b6e166d72bd
Zain Qureshi <141648191+zain1290@users.noreply.github.com>
2026-07-04 01:42:22 +0200

    Add: string-encode-and-decode - submission-2

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-2.java
@@ -0,0 +1,31 @@
+class Solution {
+
+    public String encode(List<String> strs) {
+        // String res = "";
+        StringBuilder sb = new StringBuilder();
+
+        for (String s : strs) {
+            // String block = s.length() + "@" + s;
+            // res += block;
+            sb.append(s.length()).append("@").append(s);
+        }
+
+        // return res;
+        return sb.toString();
+    }
+
+    public List<String> decode(String str) {
+        List<String> res = new ArrayList<>();
+
+        int i = 0;
+        while (i < str.length()) {
+            int j = i;
+            while (str.charAt(j) != '@') j++;
+            int num = Integer.parseInt(str.substring(i, j));
+            res.add(str.substring(j+1, j+1+num));
+            i = j + 1 + num;
+        }
+
+        return res;
+    }
+}
