1dfa605fd76b3f5cbfe2db35c81ebb0e9b8d4a5a
Zain Qureshi <141648191+zain1290@users.noreply.github.com>
2026-07-12 16:44:21 +0200

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int l = 0;
+        int r = s.length() - 1;
+
+        while (l < r) {
+            while (l < r && !isValid(s.charAt(l))) l++;
+            while (l < r && !isValid(s.charAt(r))) r--;
+            if (Character.toLowerCase(s.charAt(l)) != Character.toLowerCase(s.charAt(r))) return false;
+            l++;
+            r--;
+        }
+
+        return true;
+    }
+
+    public boolean isValid(char c) {
+        return (c >= 'A' && c <= 'Z'|| c >= 'a' && c <= 'z' || c >= '0' && c <= '9');
+    }
+}
